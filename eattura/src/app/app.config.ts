d7314ea2123101216df1ba673e896b99aedbdde1
Nico <spinanico93@gmail.com>
2026-02-14 16:32:55 +0100

    app | init first commit

--- eattura/src/app/app.config.ts
+++ eattura/src/app/app.config.ts
@@ -1,16 +1,16 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
+  provideZonelessChangeDetection,
 } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import {provideRouter} from "@angular/router";
 
-import { routes } from "./app.routes";
+import {routes} from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
   ],
 };
